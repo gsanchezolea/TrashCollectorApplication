489dd16f3d4418993cca66e46a4e9a07c040a395
unknown <thetavog@gmail.com>
2020-03-28 20:26:42 -0500

    init

---
+++ TrashCollectorApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TrashCollectorApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
